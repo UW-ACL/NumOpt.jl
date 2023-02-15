dccf9354de84730f0536c22ca0cbedd116bbabcf
Purnanand Elango <purnanandelango@gmail.com>
2023-02-14 18:23:02 -0800

    Update gradient descent

--- src/firstorder.jl
+++ src/firstorder.jl
@@ -2,8 +2,8 @@ function gradient_descent(x0,∇f::Function,α,ϵ)
     xk = similar(x0)
     xkp1 = similar(x0) .+ 100 
     while norm(xkp1 .- xk) ≥ ϵ
+        xk .= xkp1        
         xkp1 .= xk .- α .* ∇f(xk)
-        xk .= xkp1
     end
     return xkp1
 end
