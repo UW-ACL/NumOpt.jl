ce2d280acaf491ec7ba6a41b01779474d0e800d5
govindchari <govindchari1@gmail.com>
2023-03-03 15:57:14 -0800

    Added plotting

--- test/gradient_descent.jl
+++ test/gradient_descent.jl
@@ -1,5 +1,12 @@
+# using PyPlot
 include("test_functions.jl")
 
+α_list = []
+gf_list = []
+function mon(a, gf)
+    push!(α_list, a)
+    push!(gf_list, norm(gf))
+end
 n = 4
 
 a = randn(n)
@@ -11,7 +18,25 @@ x0 = randn(n)
 α = 0.1
 ϵ = 1e-7
 xopt = gradient_descent(f, ∇f, x0, ϵ)
-xopt_banana = gradient_descent(banana, banana_grad, [10.0;10.0], ϵ)
+xopt_banana = gradient_descent(banana, banana_grad, [10.0;10.0], ϵ, monitor=mon)
+
+pygui(true)
+plt.figure()
+plt.plot(α_list)
+plt.title("Stepsize vs Iteration")
+plt.xlabel("Iteration")
+plt.ylabel("Stepsize")
+plt.grid(true)
+
+plt.figure()
+plt.plot(gf_list)
+plt.yscale("log")
+plt.title("Gradient Norm vs Iteration")
+plt.xlabel("Iteration")
+plt.ylabel("Gradient Norm")
+plt.grid(true)
+
+plt.show()
 
 tol = 1e-2
 @testset "Gradient Descent" begin
